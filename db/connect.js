a4ef680249ac07758bc38e2cf6d729ed62dded15
prajwalkutwal <prajwalxkutwal@gmail.com>
2023-04-23 21:46:18 +0530

    mongo url direct

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const mongo_connect = "mongodb+srv://PrajwalK:QazWsxEdc@cluster0.x5p5u.mongodb.net/TODO_List?retryWrites=true&w=majority"
-const connectDB = (mongo_connect) => {
+const connectDB = (url) => {
   return mongoose.connect(mongo_connect, {
     useNewUrlParser: true,
     useCreateIndex: true,
